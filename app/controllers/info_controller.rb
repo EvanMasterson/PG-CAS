a65d0fde983e813c103fe09a453d3c87a67679c8
EvanMasterson <evanm95@hotmail.com>
2020-07-25 10:03:16 +0100

    switch to find_by, retrieve fresh data if existing data is more than a day old

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -2,23 +2,27 @@ class InfoController < ApplicationController
   def coviddata
     if params[:location].to_s.present?
       @location = params[:location]
-      existingData = CovidApiData.where("payload->>'location' = ?", @location)
-
+      existingData = CovidApiData.find_by("payload->>'location' = ?", @location)
       # If data already exists in DB retrieve it, else hit the API and store the response only if it has valid data
-      if !existingData.empty?
-        @response = existingData.first.payload["data"]
-        puts @response
-      else
-        @response = Api.getCovidData(@location)
-        puts @response
-        if  @response.empty? || @response.instance_of?(Hash)
-          @response = {"message"=>"Not Found"}
-        else
-          CovidApiData.create(payload: {location: @location, data: @response})
+      if !existingData.nil?
+        if  existingData.updated_at > 1.day.ago
+          existingData.update(payload: {location: @location, data: self.retrieveApiData})
         end
+        @response = existingData.payload["data"]
+      else
+        @response = self.retrieveApiData
+        CovidApiData.create(payload: {location: @location, data: @response})
       end
     else
       redirect_to(root_url)
     end
+  end
+
+  def retrieveApiData
+    apiData = Api.getCovidData(@location)
+    if apiData.empty? || apiData.instance_of?(Hash)
+      apiData = {"message"=>"Not Found"}
+    end
+    return apiData
   end
 end
