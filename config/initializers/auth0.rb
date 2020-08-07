0e25dedbf8c863d6ba46f6be321b1d6aaee9a096
EvanMasterson <evanm95@hotmail.com>
2020-08-07 12:17:22 +0100

    update to use credentials.yml.enc properly, add rds connection

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'JQ5uoA8wiuSsZ5Z34oddLSGb2dfn2sJ6',
-    ENV['AUTH0_CLIENT_SECRET'],
-    ENV['AUTH0_DOMAIN'],
+    Rails.application.credentials.dig(:auth0, :client_id),
+    Rails.application.credentials.dig(:auth0, :client_secret),
+    Rails.application.credentials.dig(:auth0, :domain),
     callback_path: '/auth/auth0/callback',
     authorize_params: {
       scope: 'openid email profile'
